81407c29b52aec543a7f5d1d0b99ae9e61ffee47
csmith-0141 <connor@gridcure.com>
2021-07-29 15:52:51 -0400

    updates init script , adds tmux plugin manger

--- executable_dot_init.sh
+++ executable_dot_init.sh
@@ -13,7 +13,7 @@ sudo apt-get -y install cmake
 sudo apt-get -y install python-dev python-pip python3-dev python3-pip
 sudo apt-get -y install zsh 
 
-chsh -d $(which zsh)
+chsh -s $(which zsh)
 
 curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -21,18 +21,21 @@ sudo apt-get install -y nodejs
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 
 git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
 gpg --keyserver keyserver.ubuntu.com --recv 0x175751A488C14868
 
-nvim --headless +PlugInstall +qall
-nvim --headless +PlugUpdate +qall
-
 git clone https://github.com/jandamm/zgenom.git
 git clone https://github.com/unixorn/zsh-quickstart-kit.git
 
 pushd . 
 cd zsh-quickstart-kit
+
 stow --target=$HOME zsh
+popd
+
+nvim --headless +PlugInstall +qall
+nvim --headless +PlugUpdate +qall
